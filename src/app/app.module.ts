afb2ca00c2b793d404eb5493e791ac9144c59eec
leonardo mengesha <leomengesha@projectzeta.co.uk>
2019-08-27 12:04:58 +0100

    added structure to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SnippetComponent } from './pages/karenda-project/components/snippet/snippet.component';
+import { RightPanelComponent } from './pages/karenda-project/components/right-panel/right-panel.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnippetComponent,
+    RightPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
