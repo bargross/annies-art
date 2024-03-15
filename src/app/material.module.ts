f342df6b8d6cd7ca74daa4376b2306fd051bf39d
Annie <anam.k@live.com>
2024-03-15 22:01:50 +0000

    updates

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,9 @@
-import { NgModule } from '@angular/core';
-
-import {
-  MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-} from '@angular/material';
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   imports: [
@@ -14,14 +11,14 @@ import {
     MatMenuModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
-  ]
+    MatCardModule,
+  ],
 })
 export class MaterialModule {}
